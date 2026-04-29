48350e882b202b9198195a6ac1dbbf6e9cbc341f
botheis <nkneib@medulla-tech.io>
2026-04-29 19:31:18 +0200

    improove the config reader
    The get_config function had some issues on overriding the configuration.

--- diag/utils.py
+++ diag/utils.py
@@ -7,24 +7,18 @@ def get_config(path, local=".local"):
     config = None
     if os.path.exists(path):
         config = ConfigParser()
-        config.read(path)
+        with open(path, "r") as f:
+            config.read_file(f)
     else:
         return None
 
     localfile = path+local
 
-    localconfig = None
     if os.path.exists(localfile):
         # Override loaded config with local one
-        localconfig = ConfigParser()
-        localconfig.read(localfile)
+        with open(localfile, "r") as f:
+            config.read_file(f)
 
-        for section in localconfig.sections():
-            for option in localconfig.options(section):
-                try:
-                    config.set(section, option, localconfig.get(section, option))
-                except:
-                    config.set(section, option, "")
     return config
 
 def project_dir():
